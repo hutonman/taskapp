700200309831c8cb68c17fe7aa0164c7f3260d29
hutonman <hutonman051@gmail.com>
2022-03-27 20:34:35 +0900

    UPDATE form styling

--- resources/views/add-task.blade.php
+++ resources/views/add-task.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="mx-auto sm:px-6 lg:px-8" style="max-width: 30rem">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                   <form action="/add-task" method="post">
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <form action="{{ route('search') }}" method="post" class="mb-4">
                 @csrf
-                <x-input id="search" type="text" name="search"></x-input>
+                <x-input id="searwch" type="text" name="search" style="width: 12rem"></x-input>
                 <x-button>{{ __('検索') }}</x-button>
             </form>
             @if (isset($keyword))
@@ -20,17 +20,17 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <div class="flex justify-between">
-                        <div>
+                        <div class="text-sm">
                             <input type="checkbox" name="over" id="over" @if( isset($over) && $over ) checked @endif />
                             <x-label-inline for="over">{{ __('期限超過のみ') }}</x-label>
                             <input type="checkbox" name="status" id="status" @if( isset($status) && $status ) checked @endif />
                             <x-label-inline for="status">{{ __('完了済み表示') }}</x-label>
                         </div>
-                        <div>
+                        <div class="text-sm">
                             <form action="/add-task" method="get"><x-button>{{ __('＋タスク追加') }}</x-button></form>
                         </div>
                     </div>
-                    <div class="flex mb-4 text-xl">
+                    <div class="flex mb-4 text-md sm:text-sm">
                         <div class="flex-1">名前</div>
                         <div class="flex-1">タイトル</div>
                         <div class="flex-1">期限</div>
