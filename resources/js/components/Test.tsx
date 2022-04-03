cad2a87be3b28cd85aa1bc5eaeff0b9f322a5ef5
hutonman <hutonman051@gmail.com>
2022-04-03 10:04:38 +0900

    Installed Typescript

---
+++ resources/js/components/Test.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export const Test = () => {
+  let str: string;
+  let num: number;
+
+  str = "3";
+  console.log(str);
+
+    return <div>test</div>;
+};
